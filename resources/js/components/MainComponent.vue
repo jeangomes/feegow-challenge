6b255dcb07c00558b710726b9ea545d551787477
Jean Gomes <jeangomes.ti@gmail.com>
2022-05-15 12:39:00 -0300

    first commit

---
+++ resources/js/components/MainComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>{{name}}</h1>
+        <label for="city_id">Município <sup>*</sup></label>
+        <select id="city_id" class="form-control">
+            <option value="">-- selecione --</option>
+        </select>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "MainComponent",
+    data() {
+        return {
+            name: 'Jean Gomes',
+            idade: 31
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
